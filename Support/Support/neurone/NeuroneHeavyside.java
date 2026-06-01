db084913dcfb042d712fc8cc022a3d41629c5067
Robin Nullans <robinnullans@gmail.com>
2026-06-01 10:05:39 +0200

    Image1

--- Support/Support/neurone/NeuroneHeavyside.java
+++
@@ -1,8 +0,0 @@
-public class NeuroneHeavyside extends Neurone
-{
-	// Fonction d'activation d'un neurone (peut facilement être modifiée par héritage)
-	protected float activation(final float valeur) {return valeur >= 0 ? 1.f : 0.f;}
-	
-	// Constructeur
-	public NeuroneHeavyside(final int nbEntrees) {super(nbEntrees);}
-}
